2c10996866fee5434825c3c580523818abd48a2d
RuKi <ruki00801@gmail.com>
2024-03-26 23:00:18 +0900

    player damage
    플레이어 데미지 구현, 폴더 정리

---
+++ Assets/Model/Player/Code/playerDamage.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerDamage : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "box" || other.tag == "book") 
+        {
+            float damage = other.GetComponent<Rigidbody>().velocity.magnitude;
+            
+            if(damage > 1) print("BOOM!!!");
+        }
+    }   
+}   
