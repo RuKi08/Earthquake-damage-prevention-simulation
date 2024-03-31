b0cdd7ad6ae3261da0a1fbb259cf4c5fea1eb267
RuKi <ruki00801@gmail.com>
2024-03-31 19:44:15 +0900

    add DNA
    유전학습을 위한 DNA를 추가하고, 시간에 따른 DNA를 통한 이동을 구현하였다.

--- Assets/Model/Player/Code/playerMove.cs
+++ Assets/Model/Player/Code/playerMove.cs
@@ -4,26 +4,42 @@ using UnityEngine;
 
 public class playerMove : MonoBehaviour
 {
+    public int DNASize;
+    public float ActionInterval;
     public float playerSpeed;
 
+    public int[] DNA;
+
+    float StartTime;
     int r = 0;
+
+    void Start()
+    {
+        DNA = new int[DNASize];
+        for(int i = 0; i < DNA.Length; i++) DNA[i] = Random.Range(0,6);
+
+        StartTime = Time.time;
+    }
     
     void Update()
     {
-        int Move = 0;
+        int time = (int)((Time.time - StartTime)/ActionInterval);
+        int Move = 1;
 
-        if(Input.GetKey(KeyCode.A) 
-        || Input.GetKey(KeyCode.D) 
-        || Input.GetKey(KeyCode.W) 
-        || Input.GetKey(KeyCode.S)) Move = 1;
+        if(time < DNA.Length)
+        {
+            switch (DNA[time])
+            {
+                case 1:  r = 1;     break;
+                case 2:  r = 2;     break;
+                case 3:  r = 3;     break;
+                case 4:  r = 4;     break;
+                default: Move = 0;  break;
+            }
 
-        if      (Input.GetKey(KeyCode.A)) r = 3;
-        else if (Input.GetKey(KeyCode.D)) r = 1;
-        else if (Input.GetKey(KeyCode.W)) r = 0;
-        else if (Input.GetKey(KeyCode.S)) r = 2;
-
-        transform.Translate(Move * Vector3.right * playerSpeed * Time.deltaTime);
-        
-        transform.eulerAngles = Vector3.up * r * 90;
+            transform.Translate(Move * Vector3.right * playerSpeed * Time.deltaTime);
+            
+            transform.eulerAngles = Vector3.up * r * 90;
+        }
     }
 }
