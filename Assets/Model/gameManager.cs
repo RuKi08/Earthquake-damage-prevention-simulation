56fbc1d85913454c95910ae567952b83015ad830
RuKi <ruki00801@gmail.com>
2024-04-04 23:34:26 +0900

    genetic algorithm
    유전 알고리즘의 선택, 교차, 변이, 대치 과정을 gameManager에 추가하였다.

--- Assets/Model/gameManager.cs
+++ Assets/Model/gameManager.cs
@@ -61,6 +61,57 @@ public class gameManager : MonoBehaviour
 
         for (int i = 0; i < Count; i++)         ObjectList[i].SetActive(false);
         for (int i = 0; i < Survivor.Count; i++)ObjectList[Survivor[i]].SetActive(true);
+
+        int Survivor_Count_Num = 0;
+
+        for (int i = 0; i < Count; i++)
+        {
+            if(i == Survivor[Survivor_Count_Num]) 
+            {
+                if(Survivor_Count_Num < Survivor.Count-1) Survivor_Count_Num++;
+            }
+            else
+            {
+                int ParentDNA_num1 = Random.Range(0, Survivor.Count);
+                int ParentDNA_num2 = Random.Range(0, Survivor.Count);
+
+                int[] ParentDNA_1 = ObjectList[Survivor[ParentDNA_num1]].GetComponent<playerMove>().DNA;
+                int[] ParentDNA_2 = ObjectList[Survivor[ParentDNA_num2]].GetComponent<playerMove>().DNA;
+
+                ObjectList[i].GetComponent<playerMove>().DNA = Mutation(Heredity((ParentDNA_1, ParentDNA_2)));
+            }
+        }
+    }
+    
+    public int GeneSize;
+
+    int[] Heredity((int[],int[]) DNA)
+    {
+        int[] childDNA = new int[DNA.Item1.Length];
+
+        for(int i = 0; i < DNA.Item1.Length / GeneSize; i++)
+        {
+            bool randomBool = (Random.value > 0.5f);
+
+            if(randomBool)  for (int n = 0; n < GeneSize; n++) childDNA[i*GeneSize + n] = DNA.Item1[i*GeneSize + n];
+            else            for (int n = 0; n < GeneSize; n++) childDNA[i*GeneSize + n] = DNA.Item2[i*GeneSize + n];
+        }
+
+        return childDNA;
+    }
+
+    public int MutationSize;
+
+    int[] Mutation(int[] TargetDNA)
+    {
+        int M_size = Random.Range(0, MutationSize);
+
+        for (int i = 0; i < M_size; i++)
+        {
+            TargetDNA[Random.Range(0, TargetDNA.Length)] = Random.Range(0, 6);
+        }
+        
+        return TargetDNA;
     }
 
     void SetObject()
